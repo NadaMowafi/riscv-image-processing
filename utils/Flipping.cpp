30647ee03a6b2f1eebab25afc161bd6732e1a753
HabibaHatemm <habibahatem94@gmail.com>
2025-03-14 16:11:53 +0200

    Flipping

--- utils/Flipping.cpp
+++ utils/Flipping.cpp
@@ -1,15 +1,15 @@
 #include "Flipping.h"
+#include <vector>
 
-void ImageFlipper::(){
 
-
+void ImageFlipper::flip(Image&image, FlippingDirection direction){
 
     switch (direction) {
-        case ::Vertical:
-            flipVertical();
+        case FlippingDirection::Vertical:
+            flipVertical(image.pixelMatrix);
             break;
-        case ::Horizontal:
-            flipHorizontal();
+        case FlippingDirection::Horizontal:
+            flipHorizontal(image.pixelMatrix);
             break;
     }
 
@@ -23,26 +23,27 @@ void ImageFlipper::flipVertical(std::vector<std::vector<uint8_t>>& matrix) {
     size_t height = matrix.size();
     size_t width = matrix[0].size();
 
-    std::vector<std::vector<uint8_t>> Flipped(height, std::vector<uint8_t>(width));
+    //std::vector<std::vector<uint8_t>> Flipped(height, std::vector<uint8_t>(width));
 
     for (size_t i = 0; i < height / 2; ++i) {
         for (size_t j = 0; j < width; ++j) {
             std::swap(matrix[i][j], matrix[height - 1 - i][j]);
         }
     }
-    matrix = std::move(Flipped);
+    
 }
 
 void ImageFlipper::flipHorizontal(std::vector<std::vector<uint8_t>>& matrix) {
     size_t height = matrix.size();
     size_t width = matrix[0].size();
 
-    std::vector<std::vector<uint8_t>> Flipped(height, std::vector<uint8_t>(width));
+    //std::vector<std::vector<uint8_t>> Flipped(height, std::vector<uint8_t>(width));
     
     for (size_t i = 0; i < height; ++i) {
         for (size_t j = 0; j < width / 2; ++j) {
             std::swap(matrix[i][j], matrix[i][width - 1 - j]);
         }
     }
-    matrix = std::move(Flipped);
+    
 }
+
--- utils/Flipping.hpp
+++ utils/Flipping.hpp
@@ -1,1 +1,25 @@
-#include
+#include "image.hpp"
+#include <vector>
+
+enum class FlippingDirection
+{
+
+    Vertical,
+    Horizontal
+
+};
+
+class ImageFlipper 
+{
+
+public:
+   void flip(Image&image, const FlippingDirection direction);
+
+private:
+   void flipVertical(std::vector<std::vector<uint8_t>>& matrix);
+   void flipHorizontal(std::vector<std::vector<uint8_t>>& matrix);
+
+};
+
+#endif
+
